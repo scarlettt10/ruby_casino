e137a70b1f12c4205d280e47bcd235eec035b010
Robert Scarlett <scarlettt10@msn.com>
2017-08-21 10:29:46 -0600

    added high low and adde menu to casino

--- casino.rb
+++ casino.rb
@@ -2,16 +2,73 @@ require 'pry'
 require 'colorize'
 
 require_relative 'player'
+require_relative 'high_low'
 
 class Casino
 	attr_accessor :player
 
 	def initialize
 		puts "Welcome to the RUBY CASINO"
-		@player = Player.new
-		binding.pry
-	end
+    @player = Player.new
+    Highlow.new(@Player)
+    menu
+		
+  end
+  
+  def menu
+    puts "1) Play HighLow"
+    puts "2) Quit"
+
+      case gets.to_i
+        when 1
+          HighLow.new(@player)
+        when 2
+          puts "Thanks for playing"
+          exit
+        else 
+          puts "Invlaid Choice! Try again!"
+          menu
+      end
+    menu
+    end  
 end
 
 Casino.new
 
+
+=begin
+#teacher code from slack
+
+require 'pry'
+require_relative 'player'
+require_relative 'high_low'
+
+class Casino
+  attr_accessor :player
+
+  def initialize
+    puts 'Welcome To The Ruby Casino'
+    @player = Player.new
+    menu
+  end
+
+  def menu
+    puts "1) Play HighLow"
+    puts "2) Quit"
+    case gets.to_i
+      when 1
+        HighLow.new(@player)
+      when 2
+        puts 'Thanks for playing'
+        exit
+      else
+        puts 'Invalid Choice! Try Again!'
+        menu
+    end
+    menu
+  end
+end
+
+Casino.new
+
+=end
---
+++ high_low.rb
@@ -0,0 +1,13 @@
+class Highlow
+  def initalize(player)
+    puts "Welcome to High Low #{player.name}"
+    puts "You have #{player.wallet.amount} to bet with"
+    #ask the player for bet
+    #error checking player can't bet with more than they have
+    #play the game
+    #add or subtract from players wallet after game
+    #allow to play again or go back to main menu
+
+    
+  end
+end
